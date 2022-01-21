9a9c61b95954b73c8d230a0c65de9458ceda1c7c
Vinogradov Alexander <a3vinogradov@gmail.com>
2022-01-21 23:26:55 +0300

    save

--- KitchenTimer/TimerController.cpp
+++ KitchenTimer/TimerController.cpp
@@ -1,7 +1,17 @@
 #include "TimerController.h"
+CTimerController::CTimerController()
+{
+  _state = STATE_OFF;
+}
+
+void CTimerController::SetStateOFF()
+{
+  ;
+}
+
 void CTimerController::Setup()
 {
-     
+  SetStateOFF();   
 }
 
 void CTimerController::Exec()
--- KitchenTimer/TimerController.h
+++ KitchenTimer/TimerController.h
@@ -1,9 +1,21 @@
 #ifndef file_TimerController
 #define file_TimerController
 
+#define STATE_OFF 0
+#define STATE_SET_SECOND 1
+#define STATE_SET_MINUTE 2
+#define STATE_TIMER_ON 3
+#define STATE_TIMER_PAUSE 4
+
 class CTimerController
 {
+  private:
+    byte _state;
+
+    void SetStateOFF();
+  
   public:
+  CTimerController();
   void Setup();
   void Exec();
 };
